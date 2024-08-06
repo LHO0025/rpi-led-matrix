8bddcd68e6f072cf8a81814e5510e57c6d91873c
LHO0025 <kuba.lhotsky@gmail.com>
2024-08-06 12:37:23 +0200

    sdfsdf

--- spotify.py
+++ spotify.py
@@ -44,7 +44,7 @@ def thread_function(matrix):
                 pos = matrix.width
 
             time.sleep(0.05)
-        # offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
 
 x = threading.Thread(target=thread_function, args=[matrix])
 x.start()
