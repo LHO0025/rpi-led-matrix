ac02fca759fff034e40ed7761224995796ecab35
LHO0025 <kuba.lhotsky@gmail.com>
2025-08-30 11:54:47 +0200

    viewer update

--- viewer.py
+++ viewer.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 import time
 import sys
-
+import os
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
+# Folder containing images
+image_folder = "matrix_images"
 
-image = Image.open(image_file)
+# Load all image paths
+if not os.path.isdir(image_folder):
+    sys.exit(f"Folder '{image_folder}' not found")
+
+image_files = [os.path.join(image_folder, f) for f in os.listdir(image_folder)
+               if f.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif"))]
+
+if not image_files:
+    sys.exit("No image files found in folder")
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
@@ -20,14 +26,16 @@ options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'regular'
 
-matrix = RGBMatrix(options = options)
-image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-
-matrix.SetImage(image.convert('RGB'))
+matrix = RGBMatrix(options=options)
 
 try:
     print("Press CTRL-C to stop.")
     while True:
-        time.sleep(100)
+        for image_file in image_files:
+            print(f"Displaying {image_file}")
+            image = Image.open(image_file)
+            image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            matrix.SetImage(image.convert('RGB'))
+            time.sleep(15)  # show each image for 15 seconds
 except KeyboardInterrupt:
     sys.exit(0)
